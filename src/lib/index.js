872ed7accdc58453b9f57279b86793b77a9a3729
ripleyaffect <t.ripley.peterson@gmail.com>
2018-10-10 14:11:31 -0500

    Add build:program

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   createFileUuids: require('./createFileUuids'),
   expandModuleCheckpoints: require('./expandModuleCheckpoints'),
+  expandProgramModules: require('./expandProgramModules'),
   getLibraryFile: require('./getLibraryFile'),
   getLibraryFiles: require('./getLibraryFiles'),
   getYamlFile: require('./getYamlFile'),
