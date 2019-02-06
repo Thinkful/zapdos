fadeb4cfade82a7c1a2b6bd9874456411183fdd7
ripleyaffect <t.ripley.peterson@gmail.com>
2019-02-05 17:06:14 -0700

    Validate team values

--- src/lib/validateCheckpoint.js
+++ src/lib/validateCheckpoint.js
@@ -1,3 +1,5 @@
+const VALID_TEAM_VALUES = ['careers', 'grading'];
+
 const formatDuplicates = duplicates =>
   `[${duplicates.map(dup => `"${dup.attributes.title}"`).join(', ')}]`;
 
@@ -7,11 +9,20 @@ module.exports = async (checkpoint, checkpoints = null) => {
 
   let error = null;
 
-  // Check for errors
+  // Check for missing uuid
   if (!checkpoint.attributes.uuid) {
     error = `CHECKPOINT_INVALID: Checkpoint "${
       checkpoint.src
     }" has no uuid. Run uuids command`;
+  }
+
+  // Check for valid team
+  if (VALID_TEAM_VALUES.indexOf(checkpoint.attributes.team) === -1) {
+    error = `CHECKPOINT_INVALID: Checkpoint ${
+      checkpoint.src
+    } has invalid team "${
+      checkpoint.attributes.team
+    }". Valid team values: ${VALID_TEAM_VALUES}`;
   }
 
   // Check for a unique uuid
