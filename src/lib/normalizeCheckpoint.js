23f9738da0b67b6032fc4633c1b209ecfdc72d67
Kara Leary <learykara@gmail.com>
2019-06-18 08:25:19 -1000

    fix object key names

--- src/lib/normalizeCheckpoint.js
+++ src/lib/normalizeCheckpoint.js
@@ -9,8 +9,6 @@ module.exports = async checkpoint => {
   // Ensure all attributes are present
   checkpoint.attributes = Object.assign(
     {
-      // Keep raw time attribute for better error reporting
-      rawTime: checkpoint.attributes.time || null,
       // Alias name to title if not set
       title: checkpoint.attributes.name || 'Checkpoint',
     },
@@ -18,8 +16,10 @@ module.exports = async checkpoint => {
     checkpoint.attributes
   );
 
-  // Translate time estimate
-  checkpoint.attributes.time = getTimeEstimateHours(checkpoint);
+  // Translate time estimate to hours
+  checkpoint.attributes.timeHours = getTimeEstimateHours(
+    checkpoint.attributes.time
+  );
 
   // Delete `name` if it's present (use `title` instead)
   if (checkpoint.attributes.name) {
